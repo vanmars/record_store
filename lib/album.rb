7e436054c867aad20f39c00fd54913ea3e6d086c
Vanessa Stewart <vstewart1213@gmail.com>
2020-09-26 16:49:13 -0700

    Add back-end logic with specs

---
+++ lib/album.rb
@@ -0,0 +1,40 @@
+class Album
+  attr_reader :id, :name #Our new save method will need reader methods.
+
+  @@albums = {}
+  @@total_rows = 0 # We've added a class variable to keep track of total rows and increment the value when an ALbum is added.
+
+  def initialize(name, id)
+    @name = name
+    @id = id || @@total_rows += 1  # We've added code to handle the id.
+  end
+
+  def self.all
+    @@albums.values()
+  end
+
+  def self.clear
+    @@albums = {}
+    @@total_rows = 0
+  end
+
+  def self.find(id)
+    @@albums[id]
+  end
+
+  def save
+    @@albums[self.id] = Album.new(self.name, self.id)
+  end
+
+  def ==(album_to_compare)
+    self.name() == album_to_compare.name()
+  end
+
+  def update(name)
+    @name = name
+  end
+
+  def delete
+    @@albums.delete(self.id)
+  end
+end
