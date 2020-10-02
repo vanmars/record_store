5c8679b305562f8a94658baacae744fcd6526a54
Vanessa Stewart <vstewart1213@gmail.com>
2020-10-01 21:26:18 -0700

    Eliminate unnecessary lines

--- spec/album_spec.rb
+++ spec/album_spec.rb
@@ -14,9 +14,9 @@ describe '#Album' do
 
   describe('#save') do
     it("saves an album") do
-      album = Album.new("Giant Steps", nil) # nil added as second argument
+      album = Album.new("Giant Steps", nil) 
       album.save()
-      album2 = Album.new("Blue", nil) # nil added as second argument
+      album2 = Album.new("Blue", nil) 
       album2.save()
       expect(Album.all).to(eq([album, album2]))
     end
